e7cbc77cc136b8646571a3094089c5c5208e0a52
William S.H. <williamdossantoshennemann@gmail.com>
2026-06-29 23:13:45 -0300

    Menu completado

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+ [SerializeField] private string nomeDaCenaParaCarregar = "Fase_Teste";
+    
+    [SerializeField] private bool isExitButton = false;
+
+    private void OnMouseDown()
+    {
+        if (isExitButton)
+        {
+            Sair();
+        }
+        else
+        {
+            Jogar();
+        }
+    }
+
+    public void Jogar()
+    {
+        SceneManager.LoadScene(nomeDaCenaParaCarregar);
+    }
+
+    public void Sair()
+    {
+        Debug.Log("Jogador clicou em Sair!");
+        Application.Quit();
+    }
+}
