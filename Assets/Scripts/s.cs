d991b186025feed392789812add05ded1337e2eb
katsarosHoy <kateserhoy@gmail.com>
2026-08-30 15:18:57 -0300

    TUDO
    Radio, dialogo, tutorial, animação do Radio, falta deixar o dialogo bonito e terminar a primeira fase e tutorial

---
+++ Assets/Scripts/s.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class DialogueBox : MonoBehaviour
+{
+    [SerializeField] private GameObject caixaDeTexto;
+    [SerializeField] private TMP_Text textoExibido;
+    [SerializeField] private string[] linhas;
+    [SerializeField] private float atrasoInicial = 5f;
+    [SerializeField] private float atrasoPorCaractere = 0.03f;
+
+    private int linhaAtual = 0;
+
+    private void Start()
+    {
+        caixaDeTexto.SetActive(false);
+        StartCoroutine(IniciarSequencia());
+    }
+
+    private IEnumerator IniciarSequencia()
+    {
+        yield return new WaitForSeconds(atrasoInicial);
+        caixaDeTexto.SetActive(true);
+        yield return StartCoroutine(DigitarLinha(linhas[linhaAtual]));
+    }
+
+    private IEnumerator DigitarLinha(string linha)
+    {
+        textoExibido.text = "";
+        foreach (char c in linha)
+        {
+            textoExibido.text += c;
+            yield return new WaitForSeconds(atrasoPorCaractere);
+        }
+    }
+
+    public void ProximaLinha()
+    {
+        linhaAtual++;
+        if (linhaAtual < linhas.Length)
+        {
+            StartCoroutine(DigitarLinha(linhas[linhaAtual]));
+        }
+        else
+        {
+            caixaDeTexto.SetActive(false);
+        }
+    }
+}
